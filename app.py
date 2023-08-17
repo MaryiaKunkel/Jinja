8ec8bd06b97a8297d6100b3422b7dea940bede3b
Maryia Kunkel <maryiakunkel@gmail.com>
2023-08-17 15:05:54 -0500

    second commit

--- app.py
+++ app.py
@@ -7,13 +7,22 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
-@app.route('/story_1')
-def story_1():
+@app.route('/story')
+def story():
     place=request.args.get('place')
     adjective=request.args.get('adjective')
     noun=request.args.get('noun')
     verb=request.args.get('verb')
     plural_noun=request.args.get('plural_noun')
+    selected_story=request.args.get('selected_story')
+
+    story_templates={
+        'Story 1': """Once upon a time in a long-ago {place}, there lived a large {adjective} {noun}. It loved to {verb} {plural_noun}.""",
+        'Story 2': '''Once upon a time in a mystical {place}, there dwelled a wise {adjective} {noun}. It delighted in {verb} {plural_noun} under the moonlight.''',
+        'Story 3': '''In a distant land known as {place}, a brave {adjective} {noun} resided. Its greatest joy was to {verb} {plural_noun} and spread laughter.''',
+        'Story 4':'''Far away, in the realm of {place}, a curious {adjective} {noun} existed. It spent its days {verb} {plural_noun} and discovering new horizons.''',
+        'Story 5':'''Once upon a time, within the enchanted {place}, lived a kind-hearted {adjective} {noun}. Its true passion was to {verb} {plural_noun} and bring joy to all.'''
+    }
 
     story=Story(
     ["place", "noun", "verb", "adjective", "plural_noun"],
@@ -28,96 +37,9 @@ def story_1():
         'plural_noun': plural_noun
     })
 
-    return render_template('story_1.html', generated_story=generated_story)
+    story_template = ""
 
-@app.route('/story_2')
-def story_2():
-    place=request.args.get('place')
-    adjective=request.args.get('adjective')
-    noun=request.args.get('noun')
-    verb=request.args.get('verb')
-    plural_noun=request.args.get('plural_noun')
+    if selected_story in story_templates:
+        story_template=story_templates[selected_story]
 
-    story=Story(
-    ["place", "noun", "verb", "adjective", "plural_noun"],
-    """In a land far away, a {adjective} {noun} lived in a {place}. It had a unique talent for {verb} {plural_noun} every day."""
-    )
-
-    generated_story = story.generate({
-        'place': place,
-        'adjective': adjective,
-        'noun': noun,
-        'verb': verb,
-        'plural_noun': plural_noun
-    })
-
-    return render_template('story_2.html', generated_story=generated_story)
-
-@app.route('/story_3')
-def story_3():
-    place=request.args.get('place')
-    adjective=request.args.get('adjective')
-    noun=request.args.get('noun')
-    verb=request.args.get('verb')
-    plural_noun=request.args.get('plural_noun')
-
-    story=Story(
-    ["place", "noun", "verb", "adjective", "plural_noun"],
-    """Long ago, in a magical {place}, there existed a {adjective} {noun}. It was known for its ability to {verb} {plural_noun} with grace."""
-    )
-
-    generated_story = story.generate({
-        'place': place,
-        'adjective': adjective,
-        'noun': noun,
-        'verb': verb,
-        'plural_noun': plural_noun
-    })
-
-    return render_template('story_3.html', generated_story=generated_story)
-
-@app.route('/story_4')
-def story_4():
-    place=request.args.get('place')
-    adjective=request.args.get('adjective')
-    noun=request.args.get('noun')
-    verb=request.args.get('verb')
-    plural_noun=request.args.get('plural_noun')
-
-    story=Story(
-    ["place", "noun", "verb", "adjective", "plural_noun"],
-    """Once upon a time, in an enchanted {place}, a {adjective} {noun} dwelled. Its favorite pastime was to {verb} {plural_noun} under the moonlight."""
-    )
-
-    generated_story = story.generate({
-        'place': place,
-        'adjective': adjective,
-        'noun': noun,
-        'verb': verb,
-        'plural_noun': plural_noun
-    })
-
-    return render_template('story_4.html', generated_story=generated_story)
-
-@app.route('/story_5')
-def story_5():
-    place=request.args.get('place')
-    adjective=request.args.get('adjective')
-    noun=request.args.get('noun')
-    verb=request.args.get('verb')
-    plural_noun=request.args.get('plural_noun')
-
-    story=Story(
-    ["place", "noun", "verb", "adjective", "plural_noun"],
-    """In a realm of {place}, a {adjective} {noun} resided. It was famous for its talent to {verb} {plural_noun} in harmony with nature."""
-    )
-
-    generated_story = story.generate({
-        'place': place,
-        'adjective': adjective,
-        'noun': noun,
-        'verb': verb,
-        'plural_noun': plural_noun
-    })
-
-    return render_template('story_5.html', generated_story=generated_story)
+    return render_template('story.html', selected_story=selected_story, generated_story=generated_story, story_template=story_template)
